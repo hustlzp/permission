d7cf1d592a66e9044509400c66387500493719ec
hustlzp <hustlzp@qq.com>
2014-09-12 17:28:17 +0800

    Translate cn comments to en comments. Oh my poor writting English...

--- permission/permission.py
+++ permission/permission.py
@@ -10,7 +10,7 @@ class Permission(object):
         self.rule = rule
 
     def __call__(self, func):
-        """提供view装饰器能力"""
+        """Give the ability to use as view decorator."""
 
         @wraps(func)
         def decorator(*args, **kwargs):
@@ -21,59 +21,61 @@ class Permission(object):
         return decorator
 
     def rule(self):
-        """为当前permission装配rule"""
+        """Add rule to this permission, must be overrided."""
         raise NotImplementedError
 
     def check(self):
-        """运行规则"""
+        """Check rules."""
         result, self.deny = self.rule.run()
         return result
 
     def show(self):
-        """显示self.rule的规则结构，调试用"""
+        """Show the structure of rule, only for debug."""
         self.rule.show()
 
 
 class Rule(object):
     def __init__(self):
         self.rules_list = [[(self.check, self.deny)]]
-        # 若子类实现了base方法，则将其返回的rule实例的rules_list串联到self.rules_list上游
+        # if subclass override base(), serial merge the return rule's
+        # rules_list to self.rules_list.
         base_rule = self.base()
         if base_rule:
             self.rules_list = Rule._and(base_rule.rules_list, self.rules_list)
 
     def __and__(self, other):
-        """逻辑与操作（&）
+        """& bitwise operation.
 
-        即将other.rules_list串联到self.rules_list的下游，
-        并返回当前实例。"""
+        Serial merge self.rules_list to other.rules_list and return self.
+        """
         self.rules_list = Rule._and(self.rules_list, other.rules_list)
         return self
 
     def __or__(self, other):
-        """逻辑或操作（|）
+        """| bitwise operation.
 
-        将self.rules_list与other.rules_list并联起来，
-        并返回当前实例"""
+        Parallel merge self.rules_list to other.rules_list and return self.
+        """
         for rule in other.rules_list:
             self.rules_list.append(rule)
         return self
 
     def show(self):
-        """显示rules_list的结构，调试用"""
+        """Show the structure of self.rules_list, only for debug."""
         for rule in self.rules_list:
             result = ", ".join([str(check) for check, deny in rule])
             print(result)
 
     def base(self):
-        """提供rule规则继承能力（串联）"""
+        """Add base rule."""
         return None
 
     def run(self):
-        """运行rules_list。
+        """Run self.rules_list.
 
-        若某一通道check通过，则返回成功状态
-        若所有通道都无法check通过，则返回失败状态（包含最后运行失败的rule的deny方法）"""
+        Return True if one rule channel has been passed.
+        Otherwise return False and the deny() method of the last failed rule.
+        """
         failed_result = None
         for rule in self.rules_list:
             for check, deny in rule:
@@ -85,16 +87,22 @@ class Rule(object):
         return failed_result
 
     def check(self):
-        """当前rule的测试方法，强制子类overload"""
+        """Codes to determine whether this rule is passed.
+
+        Must be overrided.
+        """
         raise NotImplementedError()
 
     def deny(self):
-        """当前rule测试失败后需要执行的方法，强制子类overload"""
+        """Codes to be execute when check() failed.
+
+        Must be override.
+        """
         raise NotImplementedError()
 
     @staticmethod
     def _and(rules_list_pre, rules_list_pro):
-        """将rule_list_pre串联到rule_list_pro上游"""
+        """Serial merge rule_list_pre to rule_list_pro"""
         return [rule_pre + rule_pro
                 for rule_pre in rules_list_pre
                 for rule_pro in rules_list_pro]
