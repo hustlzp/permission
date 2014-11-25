302935ff90127854647765349f3ed11920c6b7a3
hustlzp <hustlzp@qq.com>
2014-11-25 19:23:53 +0800

    Use pandoc to convert README.md to README.rst

---
+++ makedoc.py
@@ -0,0 +1,9 @@
+import pandoc
+
+pandoc.core.PANDOC_PATH = '/usr/local/bin/pandoc'
+
+doc = pandoc.Document()
+doc.markdown = open('README.md').read()
+f = open('README.rst', 'w+')
+f.write(doc.rst)
+f.close()
